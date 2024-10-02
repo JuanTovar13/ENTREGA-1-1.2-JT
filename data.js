f7f34dd1eec9ec7b018f508a9b517b09b4376f80
Juan Sebastian Tovar Monje <1006072266@u.icesi.edu.co>
2024-10-02 08:10:54 -0500

    advances

---
+++ data.js
@@ -0,0 +1,47 @@
+let p1 = {
+    product:{
+        id: 1,
+        name: "IPC-HDBW5831E-Z5E",
+        type: "8MP WDR IR Dome Network Camera",
+        price: 299.99,
+            details: {
+                resolution: "1/1.8'' 8Megapixel STARVIS™ CMOS.",
+                zoom: "33x optical zoom.",
+                tech: "Starlight+ technology.",
+                distance: " IR distance up to 500 m.",
+                autoTrack: "Auto tracking 3.0.",
+                protection: " Perimeter protection."
+            },
+        images: "https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_23692/ProductImage/1_0_01_04_23692_313550526_crop_thumb.png"
+        }
+}
+
+let p2 = {
+    product:{
+        id: 2,
+        name: "SD8A3833GA-HNV",
+        type: "8MP 33x Starlight+ IR Network PTZ Camera",
+        price: 299.99,
+        details: {
+            resolution: "1/1.8'' 8Megapixel STARVIS™ CMOS.",
+            zoom: "33x optical zoom.",
+            tech: "Starlight+ technology.",
+            distance: " IR distance up to 500 m.",
+            autoTrack: "Auto tracking 3.0.",
+            protection: " Perimeter protection."
+        },
+        image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_07_15469/ProductImage/1_0_01_07_15469_538761092_crop_thumb.png"
+
+    }
+}
+
+let p3 = {
+    
+}
+
+let p4 = {
+    
+}
+
+let data = [p1, p2, p3, p4]
+console.log(data)
---
+++ product.js
@@ -0,0 +1,16 @@
+class product {
+    constructor(id, name, type, price, resolution, zoom, tech, distance, autoT, protection, image){
+        this.id = id
+        this.name = name
+        this.type = type
+        this.price = price
+        this.resolution = resolution
+        this.zoom = zoom
+        this.tech = tech
+        this.distance = distance
+        this.autoT= autoT
+        this.protection = protection
+        this.image = image
+
+    }
+}
