fa92dea2934018b8926e13fb8749749429e9ca16
juantovar <juanstovarm@gmail.com>
2024-10-03 23:55:20 -0500

    update

--- product.js
+++ product.js
@@ -1,16 +1,20 @@
 class product {
-    constructor(id, name, type, price, resolution, zoom, tech, distance, autoT, protection, image){
+    constructor(id, name, type, price, resolution, zoom, distance, RAM, image, processor, system, channels, resolution, input, size, category){
         this.id = id
         this.name = name
         this.type = type
         this.price = price
         this.resolution = resolution
         this.zoom = zoom
-        this.tech = tech
         this.distance = distance
-        this.autoT= autoT
-        this.protection = protection
+        this.RAM= RAM
         this.image = image
+        this.processor = processor
+        this.system = system
+        this.channels = channels 
+        this.input = input
+        this.size = size
+        this.category = category 
 
     }
 }
