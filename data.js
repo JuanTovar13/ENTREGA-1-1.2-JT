1bbabfc3cf4d51a321a1010a3e9986794568ceb5
Juan Sebastian Tovar Monje <1006072266@u.icesi.edu.co>
2024-10-04 17:58:38 -0500

    update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 let p1 = {
-    product:{
+    
         id: 1,
         title: "IPC-HDBW5831E-Z5E",
         type: "8MP WDR IR Dome Network Camera",
@@ -7,37 +7,37 @@ let p1 = {
         resolution: "1/2.5” 8Megapixel progressive scan CMOS.",
         zoom: "7mm ~35mm 5X zoom lens.",
         distance: "Max. IR LEDs Length 100m",
-        RAM: "512MB.",
-        images: "https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_23692/ProductImage/1_0_01_04_23692_313550526_crop_thumb.png",
+        ram: "512MB.",
+        image: "https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_23692/ProductImage/1_0_01_04_23692_313550526_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
-        }
+        
     }
 }
 
 let p2 = {
-    product:{
+ 
         id: 2,
-        name: "SD8A3833GA-HNV",
+        title: "SD8A3833GA-HNV",
         type: "8MP 33x Starlight+ IR Network PTZ Camera",
         price: 1200000,
         resolution: "1/1.8'' 8Megapixel STARVIS™ CMOS.",
         zoom: "33x optical zoom.",
         distance: " IR distance up to 500 m.",
-        RAM: "2 GB.",
+        ram: "2 GB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_07_15469/ProductImage/1_0_01_07_15469_538761092_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p3 = {
-    product:{
+   
         id: 3,
-        name: "XVR7808S-4K-I3",
+        title: "XVR7808S-4K-I3",
         type: "8CH Penta-brid 4K 2U 8HDDs WizSense Digital Video Recorder",
         price: 800000,
         processor: "Industrial-grade processor",
@@ -49,13 +49,13 @@ let p3 = {
             id: 2,
             name: "DVR",   
         }
-    }
+    
 }
 
 let p4 = {
-    product:{
+   
         id: 4,
-        name: "DHI-LM22-H200-B4",
+        title: "DHI-LM22-H200-B4",
         type: "21.5'' FHD LED Monitor",
         price: 500000,
         resolution: "1920x1080",
@@ -67,116 +67,115 @@ let p4 = {
             id: 3,
             name: "Monitor",   
         }
-    }
+    
 }
 
 let p5 = {
-    product:{
+    
         id: 5,
-        name: "IPC-HFW71242H-Z-X",
+        title: "IPC-HFW71242H-Z-X",
         type: "12MP IR Bullet WizMind Network Camera",
         price: 1900000,
         resolution: "12-MP 1/1.7'' CMOS image sensor, low luminance, and high definition image.",
         zoom: "100x optical zoom.",
         distance: "Built-in IR LED, and the max. illumination distance is 60 m.",
-        RAM: "4 GB.",
+        ram: "4 GB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_40696/ProductImage/1_0_01_04_40696_313736725_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p6 = {
-    product:{
+    
         id: 6,
-        name: "IPC-HF71242F-Z-X",
+        title: "IPC-HF71242F-Z-X",
         type: "12MP Box WizMind Network Camera",
         price: 1900000,
         resolution: "12-MP 1/1.7'' CMOS image sensor, low luminance, and high definition image.",
         zoom: "100x optical zoom.",
         distance: "Built-in IR LED, and the max. illumination distance is 60 m.",
-        RAM: "4 GB.",
+        ram: "4 GB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_40844/ProductImage/1_0_01_04_40844_313468640_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p7 = {
-    product:{
+    
         id: 7,
-        name: "IPC-HDBW2431F-AS-S2",
+        title: "IPC-HDBW2431F-AS-S2",
         type: "4MP Lite IR Fixed-focal Dome Network Camera",
         price: 500000,
         resolution: "4-MP, 1/3” CMOS image sensor, low illuminance, high image definition.",
         zoom: "100x optical zoom.",
         distance: "Built-in IR LED, max. IR distance: 30 m.",
-        RAM: "128 MB.",
+        ram: "128 MB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_33482/ProductImage/1_0_01_04_33482_393430577-Black_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p8 = {
-    product:{
+    
         id: 8,
-        name: "IPC-HDBW5842R-ASE",
+        title: "IPC-HDBW5842R-ASE",
         type: "8MP IR Fixed-focal Vandal-proof Dome WizMind Network Camera",
         price: 800000,
         resolution: "8-MP 1/1.8'' CMOS image sensor, low luminance, and high definition image.",
         zoom: "33x optical zoom.",
         distance: "Built-in IR LED, and the max. illumination distance is 50 m.",
-        RAM: "2 GB.",
+        ram: "2 GB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_40770/ProductImage/1_0_01_04_40770_400556404-Black_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p9 = {
-    product:{
+    
         id: 9,
-        name: "IPC-HFW5842T-SE",
+        title: "IPC-HFW5842T-SE",
         type: "8MP IR Fixed-focal Bullet WizMind Network Camera",
         price: 1900000,
         resolution: " 8-MP 1/1.8'' CMOS image sensor, low luminance, and high definition image.",
         zoom: "100x optical zoom.",
         distance: "Built-in IR LED, and the max. illumination distance is 80 m.",
-        RAM: "2 GB.",
+        ram: "2 GB.",
         image:"https://material.dahuasecurity.com/uploads/cpq/prm-os-srv-res/smart/formal/Product/HQ/1_0_01_04_40803/ProductImage/1_0_01_04_40803_387513448-Black_crop_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let p10 = {
-    product:{
+    
         id: 10,
-        name: "IPC-HDW5842T-ZE-S2",
+        title: "IPC-HDW5842T-ZE-S2",
         type: "8 MP IR Vari-focal Eyeball WizMind Network Camera",
         price: 1900000,
         resolution: "8MP 1/1.8'' CMOS image sensor, low illuminance and high definition image",
         zoom: "50x optical zoom.",
         distance: "Max. IR distance: 40 m",
-        RAM: "1 GB.",
+        ram: "1 GB.",
         image:"https://material.dahuasecurity.com/uploads/image/20210928/7-PLW011-08-001_T-ZS_--__45Left-logo_12_thumb.png",
         category: {
             id: 1,
             name: "Camera",   
         }
-    }
+    
 }
 
 let data = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
-console.log(data)
--- product.js
+++ product.js
@@ -1,13 +1,14 @@
-class product {
-    constructor(id, name, type, price, resolution, zoom, distance, RAM, image, processor, system, channels, resolution, input, size, category){
+class Product {
+    constructor(id, name, title, type, price, resolution, zoom, distance, ram, image, processor, system, channels, input, size, category){
         this.id = id
         this.name = name
+        this.title = title
         this.type = type
         this.price = price
         this.resolution = resolution
         this.zoom = zoom
         this.distance = distance
-        this.RAM= RAM
+        this.ram = ram
         this.image = image
         this.processor = processor
         this.system = system
@@ -15,6 +16,18 @@ class product {
         this.input = input
         this.size = size
         this.category = category 
+        
 
+    }
+
+    htmlCard(){
+        return `
+        <div class="item">
+            <a href="./productdetails.html"> 
+            <img src="${this.image}" alt="Item Image"></a>
+            <h3>${this.title}</h3>
+            <p>${this.type}</p>
+        </div>
+        `
     }
 }
